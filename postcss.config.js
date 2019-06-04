8468a34ebcdea947140505a61a2ec70b1a60b445
Angel <harlock.ma@gmail.com>
2019-06-04 15:15:54 -0500

    add postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    "autoprefixer": {},
+    "postcss-preset-env": {
+      "browsers": "last 2 versions",
+      "features": {
+        "nesting-rules": true,
+        "custom-media-queries": true,
+        "color-mod-function": true
+      }
+    }
   }
 }
